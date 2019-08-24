8784892ad560299b1c110498daadd2315c445a26
halloleo <git@halloleo.dot.hailmail.dot.net>
2019-08-24 18:56:43 +1000

    Log to files
    * Log to files cmp-err.log etc
    * Info writing
    * Inform about exclusion patterns only once
    * Test files

--- cmpdisktree.py
+++ cmpdisktree.py
@@ -1,59 +1,122 @@
 #!/usr/bin/env python3
 
+import sys
 import os
 from pathlib import Path
 from enum import Enum, auto
 import filecmp
 import re
+import logging # only for the levels
+
+from typing import List, TextIO
 
 import click
-
 
 #
 # Helpers
 #
 class ErrorKind(Enum):
+    """Different errors that can occur"""
     NOT_EXIST_IN_1 = 'Path does not exist in DIR1'
     NOT_EXIST_IN_2 = 'Path does not exist in DIR2'
     DIFF = 'Content is different'
     NOACCESS = 'No access (e.g. file permissions)'
 
+
 class FileKind(Enum):
+    """Type of file"""
     FILE = 'File'
     DIR = 'Directory'
     SYMLINK = 'Symlink'
+
+LOG_BACKUP_EXT = '.bak'
+
+class LogFile():
+    """
+    Write information to a log file
+
+    Features:
+    - File is ONLY created when first written to
+    - Possible old file (from previous run) is ALWAYS move to *.bak
+    """
+    def __init__(self, fname):
+        self.fname = fname
+        self.fobject = None
+        pf = Path(fname)
+        if pf.is_file():
+            pf.rename(pf.with_name(pf.name+LOG_BACKUP_EXT))
+
+    def openw(self):
+        """Open file or writing"""
+        return open(self.fname, 'w')
+
+    def write(self, str):
+        if not self.fobject:
+            self.fobject = self.openw()
+        print(str, file=self.fobject)
+
+ERR_LOG_DEFAULT_NAME = 'cmp-err.log'
+OK_LOG_DEFAULT_NAME = 'cmp-ok.log'
+
+EXCLUDE_PATTERNS_DEFAULT = [
+    '/.DocumentRevisions-V100',
+    '.*/.TemporaryItems',
+    ]
+
 #
 # Main Class
 #
 class Compare():
+    """Compare two filesystems"""
 
     # list of folders not to traverse
-    exclude_patterns = ['/.DocumentRevisions-V100/.*',
-                        '/.TemporaryItems/*']
+    exclude_patterns = EXCLUDE_PATTERNS_DEFAULT
+    used_exclude_patterns = set([])
 
-    def __init__(self, dir1, dir2, report_identical=False, shallow_compare=False, structure_only=False):
+    everything_ok = True
+
+    err_log = LogFile(ERR_LOG_DEFAULT_NAME)
+    ok_log = LogFile(OK_LOG_DEFAULT_NAME)
+
+    def __init__(self, dir1, dir2,
+                 verbose=False,
+                 quiet=False,
+                 report_identical=False,
+                 structure_only=False,
+                 shallow_compare=False):
         self.dir1 = Path(dir1)
         self.dir2 = Path(dir2)
+
+        self.verbosity = logging.INFO
+        if quiet: self.verbosity = logging.ERROR
+        if verbose: self.verbosity = logging.DEBUG
+
         self.report_identical = report_identical
         self.shallow_compare = shallow_compare
         self.structure_only = structure_only
         # Initialisations
         filecmp.clear_cache()
 
-    def error(self, errkind: ErrorKind, fkind: FileKind, path1: Path, path2: Path, message: str = ''):
+    def error(self, err: ErrorKind, fkind: FileKind, path1: Path, path2: Path, message: str = ''):
         """Report errors"""
-        print (f"Error \"{errkind.value} for {fkind.value}\": '{path1}' '{path2}'")
+        self.everything_ok = False
+        self.err_log.write(f"Error \"{err.value} for {fkind.value}\": "
+                           f"'{path1}' '{path2}'")
 
     def ok(self, fkind: FileKind, path1: Path, path2: Path, message: str = ''):
         """
         Report entries which are ok
         This is normally muted"""
         if self.report_identical:
-            print (f"Identical {fkind.value}: '{path1}' '{path2}'")
+            self.ok_log.write(f"'{path1}' '{path2}'")
 
-    def info(self, message: str = ''):
-        #print(message)
-        pass
+    def echo(self, level, message, *args):
+        if level >= self.verbosity:
+            try:
+                msg = str.format(message, *args)
+            except Exception:
+                msg = message
+            click.echo(msg)
 
 
     def make_dir2_path(self, path):
@@ -97,12 +160,15 @@ class Compare():
         """Check whether PATH is exclude through the exclude_patterns"""
         for pat in self.exclude_patterns:
             if re.fullmatch(pat, path):
-                self.info(f"'{path}' excluded because of pattern {pat}")
+                if not pat in self.used_exclude_patterns:
+                    self.echo(logging.DEBUG, "Pattern '{}' used (1st time for '{}')", pat, path)
+                    self.used_exclude_patterns.add(pat)
                 return True
         return False
 
     def work(self):
         for dirpath, subdirs, filenames in os.walk(self.dir1):
+
             if not self.excluded(dirpath):
                 dirpath = Path(dirpath)
                 dir2path = self.make_dir2_path(dirpath)
@@ -112,6 +178,13 @@ class Compare():
                 else:
                     self.error(ErrorKind.NOT_EXIST_IN_2, FileKind.DIR, dirpath, dir2path)
 
+        if self.everything_ok:
+            self.echo(logging.INFO, "Compare ok.")
+            exitcode = 0
+        else:
+            self.echo(logging.INFO, f"Compare error(s) - see file '{self.err_log.fname}'.")
+            exitcode = 1
+        return exitcode
 
 
 class ExpandedPath(click.Path):
@@ -119,18 +192,26 @@ class ExpandedPath(click.Path):
         value = os.path.expanduser(value)
         return super(ExpandedPath, self).convert(value, *args, **kwargs)
 
-@click.command()
+@click.command(help=f"""Compare the directories DIR1 and DIR2 as macOS disk structures
+
+Errors are reported to a file (default '{ERR_LOG_DEFAULT_NAME}')
+""")
 @click.argument('dir1', type=ExpandedPath(exists=True))
 @click.argument('dir2', type=ExpandedPath(exists=True))
-@click.option('-s', '--report-identical', is_flag=True,
-              help="Report identical files (might result in huge output)")
-@click.option('-t', '--structure-only', is_flag=True,
+@click.option('-v','--verbose', is_flag=True,
+              help="Print debug output.")
+@click.option('-q','--quiet', is_flag=True,
+              help="No informational output.")
+@click.option('-i', '--report-identical', is_flag=True,
+              help=f"Report identical files to file (default: '{OK_LOG_DEFAULT_NAME}')")
+@click.option('-s', '--structure-only', is_flag=True,
               help="Don't compare file content")
-
 def main(*args, **kwargs):
-    """Compare the directories DIR1 and DIR2 as macOS disk structures"""
+    """Compare the directories DIR1 and DIR2 as macOS disk structures
+    """
     cc = Compare(**kwargs)
-    cc.work()
+    status = cc.work()
+    sys.exit(status)
 
 
 if __name__ == '__main__':
